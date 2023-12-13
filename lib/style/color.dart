f215a1f37704509771cfa5c5b23f090724973174
alfie <alfiey1017@gmail.com>
2023-12-13 12:46:02 +0800

    fix android

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -4,16 +4,16 @@ import 'package:flutter/material.dart';
 class ColorPlate {
   // Gray colors
   static const Color black = Colors.black;
-  static const Color darkGray = const Color(0xff4a4a4a);
-  static const Color gray = const Color(0xff9b9b9b);
-  static const Color halfGray = const Color(0xffeeeeee);
-  static const Color lightGray = const Color(0xfff5f5f4);
+  static const Color darkGray = Color(0xff4a4a4a);
+  static const Color gray = Color(0xff9b9b9b);
+  static const Color halfGray = Color(0xffeeeeee);
+  static const Color lightGray = Color(0xfff5f5f4);
   static const Color white = Colors.white;
-  static const Color clear = const Color(0);
+  static const Color clear = Color(0x00000000);
   // Colorful colors
   static const Color primaryPink = Color(0xffFA3168);
-  static const Color green = const Color(0xff1ACF0C);
-  static const Color orange = const Color(0xffFF7300);
+  static const Color green = Color(0xff1ACF0C);
+  static const Color orange = Color(0xffFF7300);
   static const Color blue = Color(0xff408CF8);
-  static const Color red = const Color(0xffFF1426);
+  static const Color red = Color(0xffFF1426);
 }
